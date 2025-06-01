9ec83654b3b28559a3a54ba505240e802a132fe8
Edem Khadiev <khadiev.edem@gmail.com>
2025-06-01 18:35:18 +0300

    feat: Add predifined range

--- projects/guess_game/src/main.zig
+++ projects/guess_game/src/main.zig
@@ -1,44 +1,84 @@
 const std = @import("std");
+
+const stdout = std.debug;
 const stdin = std.io.getStdIn().reader();
 
-const MAX_GAME_NUMBER: u32 = 100000;
-const ans: u32 = 9;
+const MAX_GAME_NUMBER: u32 = 256;
+var ans: u32 = undefined;
+
+fn read_u8() anyerror!u8 {
+    var result: u8 = undefined;
+    var line_buf: [5]u8 = undefined;
+
+    const len = stdin.read(&line_buf) catch |err| {
+        stdout.print("Error while reading from stdin. Try again. {}\n", .{err});
+        return err;
+    };
+
+    const line = std.mem.trimRight(u8, line_buf[0..len], "\r\n");
+
+    result = std.fmt.parseUnsigned(u8, line, 10) catch |err| {
+        stdout.print("Invalid number.\n", .{});
+        return err;
+    };
+
+    return result;
+}
+
+fn read_range() anyerror!std.meta.Tuple(&.{ u8, u8 }) {
+    stdout.print("Firstly you need to enter a range (e.g. from 1 to 100)\n", .{});
+    stdout.print("Enter minimal number: ", .{});
+    const min_num = read_u8() catch |err| {
+        return err;
+    };
+
+    stdout.print("Enter maximal number: ", .{});
+    const max_num = read_u8() catch |err| {
+        return err;
+    };
+
+    if (min_num >= max_num) {
+        return error.MinBiggerMax;
+    }
+    return .{ @as(u8, min_num), @as(u8, max_num) };
+}
 
 fn guessed(n: u32) bool {
     if (n == ans) {
-        std.debug.print("Congratulations!\n", .{});
+        stdout.print("Congratulations!\n", .{});
         return true;
     }
-    std.debug.print("Wrong answer :( Try again.\n", .{});
+    stdout.print("Wrong answer :( Try again.\n", .{});
     return false;
 }
 
 pub fn main() void {
-    var choice: u32 = undefined;
-    var line_buf: [100]u8 = undefined;
+    const rand = std.crypto.random;
 
-    std.debug.print("Welcome to the Guess Game!\nEnter number from 0 to {}\n", .{MAX_GAME_NUMBER});
-    while (true) {
-        line_buf = undefined;
-        choice = undefined;
-
-        std.debug.print("> ", .{});
-        const len = stdin.read(&line_buf) catch |err| {
-            std.debug.print("Error while reading from stdin. Try again. {}\n", .{err});
-            continue;
-        };
-
-        const line = std.mem.trimRight(u8, line_buf[0..len], "\r\n");
-
-        choice = std.fmt.parseUnsigned(u32, line, 10) catch {
-            std.debug.print("Invalid number.\n", .{});
-            continue;
-        };
-
-        if (choice > MAX_GAME_NUMBER) {
-            std.debug.print("Invalid number.\n", .{});
-            continue;
+    stdout.print("Welcome to the Guess Game!\n", .{});
+    const range = read_range() catch |err| {
+        switch (err) {
+            error.MinBiggerMax => {
+                stdout.print("Minimal number cannot be bigger than maximal!", .{});
+            },
+            else => {
+                stdout.print("Error while reading range!", .{});
+            },
         }
+        std.process.exit(1);
+    };
+
+    const min_num = range[0];
+    const max_num = range[1];
+
+    ans = (rand.int(u8) + min_num) % max_num;
+
+    while (true) {
+        stdout.print("> ", .{});
+        const choice = read_u8() catch {
+            stdout.print("Invalid number!", .{});
+            continue;
+        };
 
         if (guessed(choice)) {
             break;
